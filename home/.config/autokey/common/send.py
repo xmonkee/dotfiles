8b29ab2f33b5942a8b8787ef6b62bd55f1f7c54d
Mayank Mandava <mayank.mandava@afterpay.com>
2019-08-06 19:04:13 -0700

    Autokey

---
+++ home/.config/autokey/common/send.py
@@ -0,0 +1,22 @@
+terminals = ["guake", "terminal", "terminator"]
+
+
+def is_terminal(window):
+    class_name = window.get_active_class()
+    for t in terminals:
+        if class_name.find(t) != -1:
+            return True
+    return False
+
+
+def send_no_term(window, keyboard, key):
+    if not is_terminal(window):
+        keyboard.send_keys('<ctrl>+'+key)
+
+
+def send_with_term(window, keyboard, key):
+    if is_terminal(window):
+        keyboard.send_keys('<ctrl>+<shift>+'+key)
+    else:
+        keyboard.send_keys('<ctrl>+'+key)
+
